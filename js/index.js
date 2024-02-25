b4fa16cc6a1891b958b935afbf6314b26c442f2c
Matt Hallman <matthallman@gmail.com>
2024-02-25 16:37:47 -0700

    push changes

--- js/index.js
+++ js/index.js
@@ -7,10 +7,10 @@ setTimeout(fade, 3000);
 $(document).ready(function () {
     $('.workGrid button').hover(
         function () {
-            $(this).css('background-color', '#0056b3'); // Change button color on hover
+            $(this).css('background-color', '#437C90'); // Change button color on hover
         },
         function () {
-            $(this).css('background-color', '#007bff'); // Change button color back on mouseout
+            $(this).css('background-color', '#D0A646'); // Change button color back on mouseout
         }
     );
 });
